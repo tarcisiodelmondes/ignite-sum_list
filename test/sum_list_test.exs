73d73e4266296238ee9ab1ce73d6faa68f5be7ea
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-12-27 23:10:42 -0300

    Add test module SumList

---
+++ test/sum_list_test.exs
@@ -0,0 +1,14 @@
+defmodule SumListTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+      expect_response = 6
+
+      assert response == expect_response
+    end
+  end
+end
